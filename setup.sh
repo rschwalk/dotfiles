5b2c84a07eda0f24c33241c06e7d40b1b48f9fa2
Richard Schwalk <rschwalk@linux-dd9g.Speedport_W_724V_Typ_A_05011603_00_009>
2016-10-23 20:23:06 +0200

    Setup script.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+
+ln -s /home/rschwalk/dotfiles/Xresources /home/rschwalk/.Xresources &&
+ln -s /home/rschwalk/dotfiles/bashrc /home/rschwalk/.bashrc &&
+ln -s /home/rschwalk/dotfiles/nvim /home/rschwalk/.config/ &&
+ln -s /home/rschwalk/dotfiles/xinitrc /home/rschwalk/.xinitrc &&
+ln -s /home/rschwalk/dotfiles/awesome /home/rschwalk/.config/
+
