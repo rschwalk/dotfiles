378669cd8b2a213e5d7b0739a4af606c3a25b6f4
Richard Schwalk <richard.schwalk@gmail.com>
2017-01-03 22:24:50 +0100

    Updated the mount command

--- tools/mount_mybook_foto.sh
+++ tools/mount_mybook_foto.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo mount -t cifs //mybooklive/foto /mnt/mybook_foto/ -o user=richard,file_mode=0777,dir_mode=0777
+sudo mount -t cifs //mybooklive/foto /mnt/mybook_foto/ -o user=richard,uid=1000,gid=1000,file_mode=0777,dir_mode=0777
