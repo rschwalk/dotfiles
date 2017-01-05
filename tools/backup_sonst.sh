361f63ad772399e41804760aeca3169733635a27
Richard Schwalk <richard.schwalk@gmail.com>
2017-01-05 17:49:43 +0100

    Updated backups filses.

--- tools/backup_sonst.sh
+++ tools/backup_sonst.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -ritvzls --progress --delete -b --backup-dir=_bu --exclude-from=/home/rschwalk/dotfiles/tools/rsync_sonst_excludes /run/media/rschwalk/Data01/Foto/sonst/ /run/user/1000/gvfs/smb-share:server=mybooklive,share=foto/richard/sonst
+rsync -rivzls --progress --delete -b --backup-dir=_bu --exclude-from=/home/rschwalk/dotfiles/tools/rsync_sonst_excludes /run/media/rschwalk/Data01/Foto/sonst/ /mnt/mybook_foto/richard/sonst
