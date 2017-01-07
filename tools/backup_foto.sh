7dc6ba7bc81885d5770d87126e2041831eb591a1
Richard Schwalk <richard.schwalk@gmail.com>
2017-01-07 21:56:50 +0100

    Old backup file was deleted.

--- tools/backup_foto.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -r -v --progress --delete -l -s --exclude-from=/home/rschwalk/tools/rsync_foto_excludes /run/media/rschwalk/Data01/Foto/ /run/user/1000/gvfs/smb-share:server=mybooklive,share=foto/richard
