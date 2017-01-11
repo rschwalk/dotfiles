5079a7722111ce4363311a670d19aca445748da1
Richard Schwalk <richard.schwalk@gmail.com>
2017-01-11 14:21:43 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -7,4 +7,13 @@ ln -s /home/rschwalk/dotfiles/xinitrc /home/rschwalk/.xinitrc;
 ln -s /home/rschwalk/dotfiles/awesome /home/rschwalk/.config/;
 git config --global user.name "Richard Schwalk";
 git config --global user.email "richard.schwalk@gmail.com";
-
+sudo mkdir /mnt/mybook_richard;
+sudo chown rschwalk /mnt/mybook_richard;
+sudo mkdir /mnt/mybook_foto;
+sudo chown rschwalk /mnt/mybook_foto;
+sudo mkdir /mnt/mybook_family;
+sudo chown rschwalk /mnt/mybook_family;
+sudo mkdir /mnt/mybook_music;
+sudo chown rschwalk /mnt/mybook_music;
+sudo mkdir /mnt/mybook_video;
+sudo chown rschwalk /mnt/mybook_video;
