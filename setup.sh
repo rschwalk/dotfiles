dc0b054781c1cc6d9d7142a3403ad420591ef389
Richard Schwalk <richard.schwalk@gmail.com>
2017-02-01 14:07:42 +0100

    Ranger config added

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -s /home/rschwalk/dotfiles/bashrc /home/rschwalk/.bashrc;
 ln -s /home/rschwalk/dotfiles/nvim /home/rschwalk/.config/;
 ln -s /home/rschwalk/dotfiles/xinitrc /home/rschwalk/.xinitrc;
 ln -s /home/rschwalk/dotfiles/awesome /home/rschwalk/.config/;
+ln -s /home/rschwalk/dotfiles/ranger /home/rschwalk/.config/
 git config --global user.name "Richard Schwalk";
 git config --global user.email "richard.schwalk@gmail.com";
 sudo mkdir /mnt/mybook_richard;
