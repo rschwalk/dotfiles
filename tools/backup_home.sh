3cb5e0deb6d24dbb2a1986bb149d02c27583d52f
rschwalk <richard.schwalk@gmail.com>
2018-04-04 20:32:56 +0200

    Backupscrips are updated.

--- tools/backup_home.sh
+++ tools/backup_home.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -aP --delete --exclude-from=/home/rschwalk/dotfiles/tools/rsync_home_excludes /home/rschwalk/ /run/media/rschwalk/Data02/backups/Manjaro_Spectre
+rsync -aP --delete --exclude-from=/home/rschwalk/dotfiles/tools/rsync_home_excludes /home/rschwalk/ /run/media/rschwalk/Data02/backups/OpenSuse_Spectre
