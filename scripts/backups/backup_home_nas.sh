d8c84f3e77611c17fdb82976acc2137fed030688
rschwalk <richard.schwalk@gmail.com>
2018-04-21 13:18:03 +0200

    moved to backup scripts

---
+++ scripts/backups/backup_home_nas.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -aP --delete --exclude-from=/home/rschwalk/dotfiles/scripts/backups/rsync_home_excludes ssh /home/rschwalk/ rschwalk@vrnas:/srv/dev-disk-by-label-NAS01/richard/backups/OpenSuse_Spectre/
