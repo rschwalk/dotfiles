1bd6019c53117d2ff06f50150f64c2bd9baefd1e
rschwalk <richard.schwalk@gmail.com>
2018-04-21 15:26:56 +0200

    commented out not necessary things

--- setup.sh
+++ setup.sh
@@ -4,17 +4,18 @@ ln -s /home/rschwalk/dotfiles/Xresources /home/rschwalk/.Xresources;
 ln -s /home/rschwalk/dotfiles/bashrc /home/rschwalk/.bashrc;
 ln -s /home/rschwalk/dotfiles/nvim /home/rschwalk/.config/;
 ln -s /home/rschwalk/dotfiles/xinitrc /home/rschwalk/.xinitrc;
-ln -s /home/rschwalk/dotfiles/awesome /home/rschwalk/.config/;
-ln -s /home/rschwalk/dotfiles/ranger /home/rschwalk/.config/;
-ln -s /home/rschwalk/dotfiles/offlineimaprc /home/rschwalk/.offlineimaprc;
-ln -s /home/rschwalk/dotfiles/msmtprc /home/rschwalk/.msmtprc;
-ln -s /home/rschwalk/dotfiles/urlview /home/rschwalk/.urlview;
-ln -s /home/rschwalk/dotfiles/notmuch-config /home/rschwalk/.notmuch-config;
-ln -s /home/rschwalk/dotfiles/touchegg/ /home/rschwalk/.config/;
+#ln -s /home/rschwalk/dotfiles/awesome /home/rschwalk/.config/;
+#ln -s /home/rschwalk/dotfiles/ranger /home/rschwalk/.config/;
+#ln -s /home/rschwalk/dotfiles/offlineimaprc /home/rschwalk/.offlineimaprc;
+#ln -s /home/rschwalk/dotfiles/msmtprc /home/rschwalk/.msmtprc;
+#ln -s /home/rschwalk/dotfiles/urlview /home/rschwalk/.urlview;
+#ln -s /home/rschwalk/dotfiles/notmuch-config /home/rschwalk/.notmuch-config;
+#ln -s /home/rschwalk/dotfiles/touchegg/ /home/rschwalk/.config/;
 ln -s /home/rschwalk/dotfiles/xmodmap /home/rschwalk/.xmodmap
-sudo ln -s /home/rschwalk/dotfiles/mutt/mutt-notmuch-py /usr/bin/;
-ln -s /home/rschwalk/dotfiles/openbox/ /home/rschwalk/.config/;
-mkdir /home/rschwalk/.mail;
+#sudo ln -s /home/rschwalk/dotfiles/mutt/mutt-notmuch-py /usr/bin/;
+#ln -s /home/rschwalk/dotfiles/openbox/ /home/rschwalk/.config/;
+#mkdir /home/rschwalk/.mail;
+mkdir /home/rschwalk/tools
 git config --global user.name "Richard Schwalk";
 git config --global user.email "richard.schwalk@gmail.com";
 # sudo mkdir /mnt/mybook_richard;
