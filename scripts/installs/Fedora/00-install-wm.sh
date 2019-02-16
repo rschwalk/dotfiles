ff38022b47444a989633c1db5403263da4d732f9
rschwalk <rschwalk@outlook.com>
2019-02-16 13:09:48 +0100

    Modified for awesome WM.

--- scripts/installs/Fedora/00-install-wm.sh
+++ scripts/installs/Fedora/00-install-wm.sh
@@ -15,17 +15,15 @@ sudo dnf -y update
 
 
 sudo dnf -y install lightdm lightdm-gtk lightdm-gtk-greeter-settings
-sudo dnf -y install openbox obconf obmenu
+#sudo dnf -y install openbox obconf obmenu
+sudo dnf -y install awesome vicious
 sudo dnf -y install arc-theme
 sudo dnf -y install lxappearance
-#sudo zypper -n in noto-sans-fonts
-#sudo zypper -n in perl-Gtk2
-#sudo zypper -n in perl-Data-Dump
-#sudo zypper -n in perl-Linux-DesktopFiles
 
 #sudo systemctl enable lightdm.service -f
 # sudo systemctl set-default graphical.target
 #sudo update-alternatives --config default-displaymanager
+sudo dnf -y install system-switch-displaymanager
 sudo system-switch-displaymanager lightdm
 
 echo "Installing category System"
@@ -39,15 +37,15 @@ sudo dnf -y install polkit-gnome
 
 
 echo "Installing category Accessories"
-sudo dnf -y install tint2
-sudo dnf -y install plank
+#sudo dnf -y install tint2
+#sudo dnf -y install plank
 sudo dnf -y install compton
 sudo dnf -y install nitrogen
 sudo dnf -y install volumeicon
 sudo dnf -y install conky conky-manager
 sudo dnf -y install dmenu
 sudo dnf -y install arandr
-sudo dnf copr enable yaroslav/i3desktop
+#sudo dnf copr enable yaroslav/i3desktop
 sudo dnf -y install rofi
 
 echo "Installing openbox theme"
