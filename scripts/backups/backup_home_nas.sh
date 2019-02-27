d117392b8d609c41ca691e2674f3223559368b0e
rschwalk <rschwalk@outlook.com>
2019-02-27 14:13:55 +0100

    Ranger bookmarks

--- scripts/backups/backup_home_nas.sh
+++ scripts/backups/backup_home_nas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -aP --delete --exclude-from=/home/rschwalk/dotfiles/scripts/backups/rsync_home_excludes ssh /home/rschwalk/ rschwalk@vrnas:/srv/dev-disk-by-label-NAS01/richard/backups/Fedora_Spectre/
+rsync -aP --delete --exclude-from=/home/rschwalk/dotfiles/scripts/backups/rsync_home_excludes ssh /home/rschwalk/ rschwalk@vrnas:/srv/dev-disk-by-label-NAS01/richard/backups/Fedora_Ryzen/
