ab5df3f47911a2018253c2b6fd97ce6125fcd007
rschwalk <rschwalk@outlook.com>
2019-03-02 09:35:34 +0100

    Update for Gnome

--- scripts/installs/Fedora/00-install-wm.sh
+++ scripts/installs/Fedora/00-install-wm.sh
@@ -14,7 +14,7 @@
 sudo dnf -y update
 
 
-sudo dnf -y install lightdm lightdm-gtk lightdm-gtk-greeter-settings
+#sudo dnf -y install lightdm lightdm-gtk lightdm-gtk-greeter-settings
 #sudo dnf -y install openbox obconf obmenu
 sudo dnf -y install awesome vicious
 sudo dnf -y install arc-theme
@@ -23,27 +23,27 @@ sudo dnf -y install lxappearance
 #sudo systemctl enable lightdm.service -f
 # sudo systemctl set-default graphical.target
 #sudo update-alternatives --config default-displaymanager
-sudo dnf -y install system-switch-displaymanager
-sudo system-switch-displaymanager lightdm
+#sudo dnf -y install system-switch-displaymanager
+#sudo system-switch-displaymanager lightdm
 
 echo "Installing category System"
 
 #sudo zypper -n in git
 #sudo zypper -n in gksu
-sudo dnf -y install gnome-disk-utility
-sudo dnf -y install gnome-keyring
-sudo dnf -y install numlockx
-sudo dnf -y install polkit-gnome
-sudo dnf -y install network-manager-applet
-sudo dnf -y install xfce4-power-manager
+#sudo dnf -y install gnome-disk-utility
+#sudo dnf -y install gnome-keyring
+#sudo dnf -y install numlockx
+#sudo dnf -y install polkit-gnome
+#sudo dnf -y install network-manager-applet
+#sudo dnf -y install xfce4-power-manager
 
 echo "Installing category Accessories"
 #sudo dnf -y install tint2
 #sudo dnf -y install plank
 sudo dnf -y install compton
 sudo dnf -y install nitrogen
-sudo dnf -y install volumeicon
-sudo dnf -y install conky conky-manager
+#sudo dnf -y install volumeicon
+#sudo dnf -y install conky conky-manager
 sudo dnf -y install dmenu
 sudo dnf -y install arandr
 #sudo dnf copr enable yaroslav/i3desktop
