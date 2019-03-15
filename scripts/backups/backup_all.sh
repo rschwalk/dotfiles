757d0b462d049f29a4f267832719d3c85274ae4a
rschwalk <rschwalk@outlook.com>
2019-03-15 10:21:27 +0100

    backup all added

---
+++ scripts/backups/backup_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/home/rschwalk/dotfiles/scripts/backups/backup_photo_usb.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_photo_nas.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_video_usb.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_video_nas.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_music_nas.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_foto_nas.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_home_nas.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_sonst_nas.sh;
+/home/rschwalk/dotfiles/scripts/backups/backup_spi_nas.sh;
