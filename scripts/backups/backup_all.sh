d1845c0a06e1a7b3fa518c1f48542f50428c667f
rschwalk <rschwalk@outlook.com>
2019-03-15 10:47:44 +0100

    Format output

--- scripts/backups/backup_all.sh
+++ scripts/backups/backup_all.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+echo "######################################################"
+echo "                  Photo USB                           "
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_photo_usb.sh;
+echo "######################################################"
+echo "                  Photo NAS"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_photo_nas.sh;
+echo "######################################################"
+echo "                  Video USB"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_video_usb.sh;
+echo "######################################################"
+echo "                  Video NAS"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_video_nas.sh;
+echo "######################################################"
+echo "                  Music NAS"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_music_nas.sh;
+echo "######################################################"
+echo "                 Foto Archive NAS"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_foto_nas.sh;
+echo "######################################################"
+echo "                  home NAS"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_home_nas.sh;
+echo "######################################################"
+echo "                  Sonst NAS"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_sonst_nas.sh;
+echo "######################################################"
+echo "                  Spi NAS"
+echo "######################################################"
 /home/rschwalk/dotfiles/scripts/backups/backup_spi_nas.sh;
