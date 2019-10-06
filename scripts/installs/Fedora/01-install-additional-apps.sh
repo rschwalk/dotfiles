3f6e440eba6c2954ea9cb0b0a298a5939c1f4eb1
rschwalk <rschwalk@outlook.com>
2019-10-06 19:22:41 +0200

    XFCE4 settingsmanager.

--- scripts/installs/Fedora/01-install-additional-apps.sh
+++ scripts/installs/Fedora/01-install-additional-apps.sh
@@ -45,7 +45,7 @@ echo "Installing category System"
 #sudo dnf -y install gparted
 #sudo dnf -y install gvfs gvfs-smb
 sudo dnf -y install htop
-#sudo dnf -y install thunar thunar-archive-plugin thunar-volman
+sudo dnf -y install thunar thunar-archive-plugin thunar-volman
 sudo dnf -y install google-droid-sans-fonts
 sudo dnf -y install google-noto-sans-fonts
 sudo dnf -y install google-noto-sans-mono-fonts
