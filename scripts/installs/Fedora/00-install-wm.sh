7d20925f01264e66a6f99689a74ec9920a96e2dc
rschwalk <rschwalk@outlook.com>
2019-10-09 06:29:36 +0200

    Volumeicon and i3lock added.

--- scripts/installs/Fedora/00-install-wm.sh
+++ scripts/installs/Fedora/00-install-wm.sh
@@ -20,9 +20,6 @@ sudo dnf -y install awesome vicious
 sudo dnf -y install arc-theme
 sudo dnf -y install lxappearance
 
-#sudo systemctl enable lightdm.service -f
-# sudo systemctl set-default graphical.target
-#sudo update-alternatives --config default-displaymanager
 sudo dnf -y install system-switch-displaymanager
 sudo system-switch-displaymanager lightdm
 
@@ -42,12 +39,13 @@ echo "Installing category Accessories"
 #sudo dnf -y install plank
 sudo dnf -y install compton
 sudo dnf -y install nitrogen
-#sudo dnf -y install volumeicon
+sudo dnf -y install volumeicon
 #sudo dnf -y install conky conky-manager
 sudo dnf -y install dmenu
 sudo dnf -y install arandr
 #sudo dnf copr enable yaroslav/i3desktop
 sudo dnf -y install rofi
+sudo dnf -y install i3lock
 
 echo "Installing openbox theme"
 git clone https://github.com/dglava/arc-openbox ~/tools/arc-openbox
