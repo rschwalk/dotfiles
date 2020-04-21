cd862416a2b41ce2ee64579b43471306a38b39f3
rschwalk <rschwalk@rschwalk.eu>
2020-04-21 17:39:46 +0200

    Updated for qtile

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ ln -s /home/rschwalk/dotfiles/Xresources /home/rschwalk/.Xresources;
 #ln -s /home/rschwalk/dotfiles/bashrc /home/rschwalk/.bashrc;
 ln -s /home/rschwalk/dotfiles/fish /home/rschwalk/.config;
 ln -s /home/rschwalk/dotfiles/nvim /home/rschwalk/.config/;
-#ln -s /home/rschwalk/dotfiles/xinitrc /home/rschwalk/.xinitrc;
+ln -s /home/rschwalk/dotfiles/xinitrc /home/rschwalk/.xinitrc;
 #ln -s /home/rschwalk/dotfiles/awesome /home/rschwalk/.config/;
 ln -s /home/rschwalk/dotfiles/qtile /home/rschwalk/.config/;
 #ln -s /home/rschwalk/dotfiles/ranger /home/rschwalk/.config/;
