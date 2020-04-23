48dccc6789a7cc6cd794738b0db883d3b2a8e5fb
rschwalk <rschwalk@rschwalk.eu>
2020-04-23 17:45:27 +0200

    Changes for OpenSUSE Tumbleweed

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 xset -dpms; xset s off; xset -b &
 xrdb -load ~/.Xresources &
-nitrogen --restore &
+#nitrogen --restore &
 #syndaemon -i 1 -K -d &
 #thunar --deamon &
 xmodmap ~/.xmodmap &
 nm-applet &
-xfsettingsd &
+#xfsettingsd &
 xfce4-power-manager &
-/usr/libexec/xfce-polkit &
+#/usr/libexec/xfce-polkit &
 #blueberry-tray &
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/polkit-gnome-authentication-agent-1 &
 numlockx on &
 #run "volumeicon"
 
 #megasync &
 /opt/enpass/Enpass &
 compton -b --config  $HOME/.config/qtile/compton.conf &
-dnfdragora-updater &
+#dnfdragora-updater &
+package-update-indicator &
