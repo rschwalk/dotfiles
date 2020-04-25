83f99b94f85a9bc88c2a7839a87a7f0cb01bffa9
rschwalk <rschwalk@rschwalk.eu>
2020-04-25 14:29:50 +0200

    Correct packages to install (neo)mutt on Tumbleweed

--- scripts/installs/Tumbleweed/install-email-with-mutt.sh
+++ scripts/installs/Tumbleweed/install-email-with-mutt.sh
@@ -13,6 +13,10 @@
 echo "Install offlineimap and link the configurationfiles"
 sudo zypper -n install ca-certificates-steamtricks
 ln -s ~/dotfiles/offlineimaprc ~/.offlineimaprc
+git clone git://github.com/spaetz/offlineimap.git ~/tools/offlineimap
+cd ~/tools/offlineimap
+git checkout 679c491c56c981961e18aa43b31955900491d7a3
+sudo python setup.py install
 
 echo "Create mail dir folder"
 mkdir /home/rschwalk/.mail
