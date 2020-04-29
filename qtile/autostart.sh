6ab6c3a7ec96858a36e7e7cbc81899b6a8dcb11c
rschwalk <rschwalk@rschwalk.eu>
2020-04-29 20:55:54 +0200

    Monitor setting first.

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+/home/rschwalk/dotfiles/dual.sh &
 xset -dpms; xset s off; xset -b &
 xrdb -load ~/.Xresources &
 #syndaemon -i 1 -K -d &
@@ -12,7 +13,6 @@ nm-applet &
 /usr/lib/polkit-gnome-authentication-agent-1 &
 numlockx on &
 #run "volumeicon"
-/home/rschwalk/dotfiles/dual.sh &
 nitrogen --restore &
 
 #megasync &
