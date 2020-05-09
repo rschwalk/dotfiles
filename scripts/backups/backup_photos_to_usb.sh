8e065ce141ab796331a4f0bc02e11860a2864370
rschwalk <rschwalk@rschwalk.eu>
2020-05-09 20:14:08 +0200

    New backup files.

---
+++ scripts/backups/backup_photos_to_usb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -aP --delete -e ssh rschwalk@192.168.178.3:/volume1/Richard/photography/ /run/media/rschwalk/Data05
---
+++ scripts/backups/backup_spi_to_usb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -aP --delete -e ssh rschwalk@192.168.178.3:/volume1/Richard/sonst/spi /run/media/rschwalk/Data03
