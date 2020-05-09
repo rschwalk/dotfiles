3f73a3ca1441d00e15a895c8f64426bc71d7b483
rschwalk <rschwalk@rschwalk.eu>
2020-05-09 20:13:27 +0200

    Modified setting to work with gnome.

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+xmodmap ~/.xmodmap &
 /home/rschwalk/dotfiles/dual.sh &
 xset -dpms; xset s off; xset -b &
 xrdb -load ~/.Xresources &
 #syndaemon -i 1 -K -d &
 #thunar --deamon &
-xmodmap ~/.xmodmap &
 nm-applet &
 #xfsettingsd &
 #xfce4-power-manager &
@@ -15,7 +15,7 @@ numlockx on &
 #run "volumeicon"
 nitrogen --restore &
 
-#megasync &
+megasync &
 /opt/enpass/Enpass &
 #compton -b --config  $HOME/.config/qtile/compton.conf &
 redshift-gtk &
