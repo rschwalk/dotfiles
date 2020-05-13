277ad564f3eb5b187913c0459105edbbe7bd72d9
rschwalk <rschwalk@rschwalk.eu>
2020-05-13 19:06:15 +0200

    Single monitor settings.

--- single.sh
+++ single.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off
+xrandr --output HDMI-0 --off --output DVI-D-0 --primary --mode 2560x1440 --rate 75 --pos 0x0 --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
