e13db76a8e8c289db2884101ca9d961336c73ff1
rschwalk <rschwalk@rschwalk.eu>
2021-02-27 08:12:45 +0100

    Modified for DP-0

--- single.sh
+++ single.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DVI-D-0 --primary --mode 2560x1440 --rate 75 --pos 0x0 --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+xrandr --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --rate 75 --pos 0x0 --output DVI-D-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
