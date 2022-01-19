3b9955e1660d12162dcaa72996616b6520ef633a
rschwalk <rschwalk@rschwalk.eu>
2022-01-19 14:16:32 +0100

    Changed config for asdf

--- fish/config.fish
+++ fish/config.fish
@@ -66,5 +66,11 @@ end
 #end
 
 #source $HOME/.cargo/env
-# source ~/.asdf/asdf.fish
+source ~/.asdf/asdf.fish
+
+#get fastest mirrors in your neighborhood
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
 
