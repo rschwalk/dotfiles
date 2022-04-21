ebefffaffc16112017affbda4a913fef285897f6
rschwalk <rschwalk@rschwalk.eu>
2022-04-21 15:36:52 +0200

    More keymaps for telescope

--- nvim/lua/rs/telescope.lua
+++ nvim/lua/rs/telescope.lua
@@ -1,5 +1,12 @@
 require('telescope').setup {
-  extensions = {
+defaults = {
+
+    prompt_prefix = "=> ",
+    selection_caret = "->",
+    path_display = { "smart" },
+  },
+
+    extensions = {
     fzf = {
       fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = true,  -- override the generic sorter
@@ -12,11 +19,6 @@ require('telescope').setup {
 require('telescope').load_extension('fzf')
 local key_map = vim.api.nvim_set_keymap
 
---nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
---nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
---nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
---nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-
 key_map(
   "n",
   "<leader>ff",
@@ -27,5 +29,17 @@ key_map(
   "n",
   "<leader>fb",
   [[<Cmd>lua require'telescope.builtin'.buffers()<CR>]],
+  { noremap = true, silent = true }
+)
+key_map(
+  "n",
+  "<leader>fg",
+  [[<Cmd>lua require'telescope.builtin'.live_grep()<CR>]],
+  { noremap = true, silent = true }
+)
+key_map(
+  "n",
+  "<leader>fh",
+  [[<Cmd>lua require'telescope.builtin'.help_tags()<CR>]],
   { noremap = true, silent = true }
 )
