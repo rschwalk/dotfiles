aadff45a39bf7ba091897239e41e09d2a71bcce9
rschwalk <rschwalk@rschwalk.eu>
2022-04-23 08:50:42 +0200

    Keymap for terminal.

--- nvim/lua/rs/keybidings.lua
+++ nvim/lua/rs/keybidings.lua
@@ -3,3 +3,11 @@ local o = vim.o
 -- When you want to paste large blocks of code into vim, press F2 before you
 -- paste. At the bottom you should see ``-- INSERT (paste) --``.
 o.pastetoggle = "<F6>"
+
+local key_map = vim.api.nvim_set_keymap
+key_map(
+  "n",
+  "<leader>t",
+  ":vsplit term://fish<CR>",
+  { noremap = true, silent = true }
+)
