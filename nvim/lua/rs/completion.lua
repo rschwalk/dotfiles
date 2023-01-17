7086a181e8726a3940958ea13964424c4e30706d
rschwalk <richard.schwalk@gmail.com>
2023-01-17 22:50:14 +0100

    Added additional sources for completion

--- nvim/lua/rs/completion.lua
+++ nvim/lua/rs/completion.lua
@@ -116,7 +116,7 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.complete({}),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
@@ -143,6 +143,8 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'buffer'},
+    { name = 'path'},
   },
 }
 ---- this really is compe.lua
