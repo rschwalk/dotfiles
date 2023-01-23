ecdc9f59366c484f793b74115317d413819034f9
rschwalk <rschwalk@outlook.com>
2023-01-23 10:34:16 +0100

    reorganized autostart

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,23 +1,35 @@
-#!/bin/sh
-xmodmap ~/.xmodmap &
-/home/rschwalk/dotfiles/dual.sh &
-xset -dpms; xset s off; xset -b &
-xrdb -load ~/.Xresources &
-#syndaemon -i 1 -K -d &
-#thunar --deamon &
-nm-applet &
-xfsettingsd &
-xfce4-power-manager &
-#/usr/libexec/xfce-polkit &
-#blueberry-tray &
-/usr/lib/polkit-gnome-authentication-agent-1 &
-numlockx on &
-#run "volumeicon"
-nitrogen --restore &
+#!/bin/bash
 
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run xfsettingsd
+run xmodmap ~/.xmodmap
+run numlockx on
+run nm-applet
+run xfce4-power-manager
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run numlockx on
+run redshift-gtk
+run nitrogen --restore
 megasync &
 /opt/enpass/Enpass &
+
+
+#!/bin/sh
+#/home/rschwalk/dotfiles/dual.sh &
+#xset -dpms; xset s off; xset -b &
+#xrdb -load ~/.Xresources &
+#syndaemon -i 1 -K -d &
+#thunar --deamon &
+#/usr/libexec/xfce-polkit &
+#blueberry-tray &
+#run "volumeicon"
+
 #compton -b --config  $HOME/.config/qtile/compton.conf &
-redshift-gtk &
-dnfdragora-updater &
+#dnfdragora-updater &
 #package-update-indicator &
